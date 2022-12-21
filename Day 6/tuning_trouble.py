73e7ad92331bc33b0bf5d54ec166be88bd094fd6
Jace Kingston <jacekingston@verizon.net>
2022-12-20 18:31:50 -0600

    Simplified Day 6 even more with functions

--- Day 6/tuning_trouble.py
+++ Day 6/tuning_trouble.py
@@ -1,22 +1,18 @@
 input = open("input.txt", "r")
 line = input.read()
 
+def get_unique_substring(length):
+    last_data_stream_packet = 0
+    for i in range(len(line) - length):
+        if len(set(line[i:i+length])) == length:
+            last_data_stream_packet = i + length
+            break
+    return last_data_stream_packet
+
 # Part One
-last_data_stream_packet = 0
-
-for i in range(len(line) - 4):
-    if len(set(line[i:i+4])) == 4:
-        last_data_stream_packet = i + 4
-        break
-
-print("Part One Answer:", last_data_stream_packet)
+print("Part One Answer:", get_unique_substring(4))
 
 # Part Two
-for i in range(len(line) - 14):
-    if len(set(line[i:i+14])) == 14:
-        last_data_stream_packet = i + 14
-        break
-
-print("Part Two Answer:", last_data_stream_packet)
+print("Part Two Answer:", get_unique_substring(14))
 
 input.close()
