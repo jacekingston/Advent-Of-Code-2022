eede38cbbaa5c3d8439e158603129b62c5f0a232
Jace Kingston <jacekingston@verizon.net>
2022-12-20 18:27:32 -0600

    Simplified Day 6

--- Day 6/tuning_trouble.py
+++ Day 6/tuning_trouble.py
@@ -5,7 +5,7 @@ line = input.read()
 last_data_stream_packet = 0
 
 for i in range(len(line) - 4):
-    if len(set([line[i], line[i+1], line[i+2], line[i+3]])) == 4:
+    if len(set(line[i:i+4])) == 4:
         last_data_stream_packet = i + 4
         break
 
@@ -13,10 +13,7 @@ print("Part One Answer:", last_data_stream_packet)
 
 # Part Two
 for i in range(len(line) - 14):
-    start_of_message = ""
-    for j in range(14):
-        start_of_message += line[i+j]
-    if len(set(start_of_message)) == 14:
+    if len(set(line[i:i+14])) == 14:
         last_data_stream_packet = i + 14
         break
 
